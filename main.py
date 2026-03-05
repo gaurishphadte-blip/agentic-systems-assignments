bdb206215dd287d9b0e3c6cfd13d39fc125e9831
G P <gaurishphadte@gmail.com>
2026-03-05 23:07:19 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from typing import Optional
-
-app = FastAPI()
-
-
-@app.get("/search")
-def search(name: Optional[str] = None, age: Optional[int] = None):
-    return {"name": name, "age": age}
